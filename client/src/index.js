6e112fbe672e8863f503654ce84eeeac2af8c1b7
Peveylun <cobzikl@gmail.com>
2023-02-16 21:12:05 +0200

    script for eslint

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import App from './components/app/App';
 import './index.css';
-import './utils/normalize.css'
+import './utils/normalize.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
