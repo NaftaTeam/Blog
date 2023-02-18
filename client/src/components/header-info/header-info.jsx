f976e64c2b0666bea76071ed88e26d8d1783b624
Peveylun <cobzikl@gmail.com>
2023-02-19 00:26:33 +0200

    some rules for eslint

--- client/src/components/header-info/header-info.jsx
+++ client/src/components/header-info/header-info.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './header-info.css';
 
 export default function HeaderInfo() {
-    return (
-        <div className='header-info'>
-            This is the first NaftaTeam project.
-        </div>
-    );
+  return (
+    <div className="header-info">
+      This is the first NaftaTeam project.
+    </div>
+  );
 }
